bff19d162d70e861b5cdb4c017921dbd6b1afa76
Chris Rush <crush@nacmtampa.com>
2015-02-06 23:52:38 -0500

    Chat changes

---
+++ src/test/groovy/com/ocdsoft/bacta/swg/cu/CuSetupSharedFile.groovy
@@ -0,0 +1,14 @@
+package com.ocdsoft.bacta.swg.cu
+
+import spock.lang.Specification
+
+/**
+ * Created by crush on 2/2/15.
+ */
+class CuTemplateWriterSpecDebug extends Specification {
+
+    def "SetupSharedFile"() {
+
+    }
+
+}
